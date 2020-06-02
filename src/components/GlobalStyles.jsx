c10023f60b7ab76a61624cc6750c974ded2470b2
SeungHyunMin <contact@seunghyun.in>
2020-06-02 23:20:17 +0900

    feat(styles): create GlobalStyles

---
+++ src/components/GlobalStyles.jsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+:root {
+  --primary-color: #376ce6;
+  --secondary-color: #28287b;
+}
+`;
+
+export default GlobalStyles;
