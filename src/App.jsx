4cbc64f352c918be2c60987d35f791065637ef85
SeungHyunMin <contact@seunghyun.in>
2020-06-02 23:20:56 +0900

    feat: create App, index

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalStyles from './components/GlobalStyles';
+import LandingPage from './pages/Landing';
+
+const App = () => (
+  <>
+    <GlobalStyles />
+    <LandingPage />
+  </>
+);
+
+export default App;
