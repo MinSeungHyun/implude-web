1e2e3e2a1fd3707a6c68bfaa3f8e3f36a565b277
SeungHyunMin <contact@seunghyun.in>
2020-06-08 18:34:58 +0900

    feat: create App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import GlobalStyles from './components/GlobalStyles';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LandingPage from './pages/Landing';
 
 const App = () => (
-  <>
+  <Router>
     <GlobalStyles />
-    <LandingPage />
-  </>
+
+    <Switch>
+      <Route exact path="/">
+        <LandingPage />
+      </Route>
+    </Switch>
+  </Router>
 );
 
 export default App;
