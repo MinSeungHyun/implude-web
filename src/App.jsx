fec55ba39906c83615ba11d8326ef22e45143049
SeungHyunMin <contact@seunghyun.in>
2020-06-27 21:55:03 +0900

    feat(page): create history page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import GlobalStyles from './components/GlobalStyles';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LandingPage from './pages/Landing';
 import AboutPage from './pages/About';
+import HistoryPage from './pages/History';
 
 const App = () => (
   <Router>
@@ -15,6 +16,10 @@ const App = () => (
 
       <Route exact path="/about">
         <AboutPage />
+      </Route>
+
+      <Route exact path="/history">
+        <HistoryPage />
       </Route>
     </Switch>
   </Router>
---
+++ src/pages/History.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+const HistoryPage = () => (
+  <>
+    <NavBar />
+  </>
+);
+
+export default HistoryPage;
