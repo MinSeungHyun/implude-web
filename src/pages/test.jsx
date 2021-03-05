677e93772900badbb6ef29361aa9729187f70b45
Gijuno <46197097+Gijuno@users.noreply.github.com>
2021-03-05 22:32:51 +0900

    Update test.jsx

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,1 +1,9 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import styled, { createGlobalStyle } from 'styled-components';
+import MediaQuery from 'react-responsive';
+import { Constants } from '../components/GlobalStyles';
 
+const LandingPage = () => (
+  <img src=""></img>
+  }
